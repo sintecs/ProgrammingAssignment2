803090a17c211aa7b1189e80970b13f9619b02ce
Dave Bucholtz <dave@bucholtz.org>
2015-07-18 14:07:09 -0400

    Code to invert matrix
    Programming assignment 2, code to invert matrix and cache result

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+##------------------------------------------------------------
+## Assignment 2
+## These functions can be used to create a matrix and
+## store the matrix and its inverse in a "cache"
+## This is useful in case the matrix and its inverse
+## need to be used multiple times as the cache of the
+## inverse negates requiring calculation of the inverse
+## on each use and instead uses the pre-computed, "cached"
+## copy
 
-## Write a short comment describing this function
+## Example usage:
+## myMat <- matrix(c(1, 1, -3, 0, 1, 0, 4, 6, -10),nrow = 3,ncol = 3)
+## myMatTest <- makeCacheMatrix(myMat)
+## cacheSolve(myMatTest)
+##------------------------------------------------------------
+
+##------------------------------------------------------------
+## makeCacheMatrix
+## This function will create a cache of the matrix passed into
+## the function.  It also contains the code to change the
+## cached matrix, calculate the inverse and store the inverse
+## of the cached matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        set <- function(y){
+                x <<- y
+                inverse <- NULL
+        }
+        get <- function(){
+                x
+        }
+        setinverse <- function(inv){
+                inverse <<- inv
+        }
+        getinverse <- function(){
+                inverse
+        }
+        list( set = set
+             ,get = get
+             ,setinverse = setinverse
+             ,getinverse = getinverse
+            )
 }
 
-
-## Write a short comment describing this function
+##------------------------------------------------------------
+## cacheSolve
+## This function will return an inverse matrix of the passed
+## matrix if a cached version does not exist.  If an inverse
+## exists in the cache, this cached version will be returned
+## instead of recalculating the inverse and a message noting
+## the cache was used will be printed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)){
+                print("Getting Cached Inverted Matrix")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setinverse(inv)
+        inv
 }
